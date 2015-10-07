51fa333e4f8d9515d11b0a726e441e6bb4c31e3c
Kristoffer Lundberg <kristoffer@bleed.com>
2015-10-07 22:41:00 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+module.exports = function(grunt) {
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	var mozjpeg = require('imagemin-mozjpeg');
+
+	grunt.initConfig({
+
+		postcss: {
+			options: {
+				map: true, // inline sourcemaps
+
+				// or
+				map: {
+					inline: false, // save all sourcemaps as separate files...
+					annotation: 'css/maps/' // ...to the specified directory
+				},
+
+				processors: [
+					require('pixrem')(), // add fallbacks for rem units
+					require('autoprefixer-core')({browsers: 'last 2 versions'}), // add ven	prefixes
+					require('cssnano')() // minify the result
+				]
+			},
+			dist: {
+				src: 'css/*/*.css'
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'css/sass/style.css': 'src/sass/main.scss'
+				}
+			}
+		},
+		stylus: {
+			build: {
+				options: {
+					linenos: false,
+					compress: false
+				},
+				files: {
+					'css/stylus/style.css': 'src/stylus/main.styl'
+				}
+			}
+		},
+		uglify: {
+			js: {
+				src: ['src/js/**/*.js'],
+				dest: 'js/main.js'
+			}
+		},
+		imagemin: {
+			dynamic: {
+				options: {
+					optimizationLevel: 7,
+					svgoPlugins: [{ removeViewBox: false }],
+					use: [mozjpeg()]
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/assets',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'assets/'
+				}]
+			}
+		},
+
+		watch: {
+			css: {
+				files: ['src/sass/**/*.scss'],
+				tasks: ['sass', 'postcss']
+			},
+			stylus: {
+				files: ['src/stylus/**/*.styl'],
+				tasks: ['stylus:build', 'postcss']
+			},
+			js: {
+				files: ['src/js/**/*.js'],
+				tasks: ['uglify:js']
+			},
+			img: {
+				files: ['src/assets/*'],
+				tasks: ['imagemin']
+			}
+		}
+	});
+
+	grunt.registerTask('default', 'watch');
+
+};
